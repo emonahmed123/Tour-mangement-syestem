a1751a2449a2ee2b2b5f6eae50d97f9b84de154a
emonahmed123 <imonshomon@gmail.com>
2022-10-03 15:36:28 +0600

    test3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+
 const cors = require('cors');
 require("dotenv").config()
 const mongoose = require('mongoose')
@@ -11,7 +11,7 @@ const tourRoutes = require('./routes/tourroutes')
 
 app.use(express.json())
 app.use(cors())
-
+const port = process.env.PORT||5000
 
 // database connection
 
