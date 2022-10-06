29ecd909cbfecf7d704b31a5d69aa61686ea6b90
emonahmed123 <imonshomon@gmail.com>
2022-10-06 09:05:04 +0600

    solve database problem

--- index.js
+++ index.js
@@ -2,27 +2,30 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors');
-require("dotenv").config()
+require("dotenv").config();
 const mongoose = require('mongoose')
 const tourRoutes = require('./routes/tourroutes')
-
+const port = process.env.PORT||5000
 
 //middlewares
 
 app.use(express.json())
 app.use(cors())
-const port = process.env.PORT||5000
 
-// database connection
-
-mongoose.connect('mongodb://localhost:27017/tour-mangement')
-.then(() => {
-  console.log(`database coonction is successfull`)
-})
-.catch((error) => console.log(error));
 
 
 app.use('/api/v1/tours', tourRoutes)
+
+
+// database connection
+
+mongoose.connect(`mongodb+srv://${process.env.DB_EMON_AHMED}:${process.env.DB_PASS}@cluster0.1uacied.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+  console.log(`database coonction is successfull`)
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
